b261f41d36f3811859745a2eedf4cbd94e7222aa
sunnyCN34 <ssahu@brightcove.com>
2017-11-24 17:01:46 -0500

    edge for ima

--- automation_test.js
+++ automation_test.js
@@ -16,3 +16,6 @@ JSON.stringify(volumeCheck, undefined, 2)
 document.querySelector('.vjs-ima3-ad-container iframe')
 
 //special for edge case
+
+
+PT_BROWSER=edge PT_OS=“windows” PT_OS_VERSION=10 BROWSER_STACK_USERNAME=witmtn1 BROWSER_STACK_ACCESS_KEY=2joZuEHyWZcHe3Sdp7zE ANALYTICS_URL=http://127.0.0.1:9292/tracker SPEC=analytics npm test
