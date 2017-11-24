773bd82de0e817beb927eff8a8d05f803a1e007d
sunnyCN34 <ssahu@brightcove.com>
2017-11-24 17:00:14 -0500

    automation npm commands

--- automation_test.js
+++ automation_test.js
@@ -1,1 +1,16 @@
 //All automation commands for player
+
+
+Automation commands
+
+BIGQUERY=bg npm start
+
+SPEC=ima3 ANALYTICS_URL=//localhost:9292/tracker npm test
+http://localhost:9292/automation
+ http://witmtn-ci.vidmark.local:9494/automation
+
+JSON.stringify(volumeCheck, undefined, 2)
+
+
+
+document.querySelector('.vjs-ima3-ad-container iframe')
